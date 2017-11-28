edb0eaa6c2c5bb104caddf3b36d79ce03b6f654f
Chong Qiang Chew <chewchongqiang@gmail.com>
2017-11-28 18:02:52 +0800

    updated CORS

--- config/application.rb
+++ config/application.rb
@@ -37,14 +37,5 @@ module Ordiez
       end
     end
 
-    # Rails 3/4
-
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-    
   end
 end
